0b1886f69eaa51210eb89579a504916926d021cb
Akshay Nair <phenax5@gmail.com>
2023-12-17 13:36:30 +0530

    feat: pretty clock. nice

---
+++ lua/sidekick/clock.lua
@@ -0,0 +1,88 @@
+local font = require('sidekick.font')
+
+local M = {
+  buffer = nil,
+  timer = nil
+}
+
+function M.configure(c)
+  M.buffer = c.buffer
+end
+
+function M.start()
+  if M.timer ~= nil then
+    M.timer:stop()
+  end
+
+  M.timer = M.timer or vim.loop.new_timer()
+  M.timer:start(0, 500, vim.schedule_wrap(function()
+    M.update_time()
+  end))
+end
+
+local function lines(str)
+  local t = {}
+  for line in str:gmatch('[^\r\n]+') do
+    table.insert(t, line)
+  end
+  return t
+end
+
+function M.str_to_glyph(str)
+  local glyphs = {}
+  for ch in str:gmatch('.') do
+    if font[ch] == nil then
+      print('missing glyph for ' .. ch)
+      return
+    end
+
+    table.insert(glyphs, lines(font[ch]))
+  end
+
+  return glyphs
+end
+
+function M.glyph_lines(glyphs, get_padding_x, padding_y)
+  local lines = {}
+  for i = 1, padding_y do
+    table.insert(lines, '')
+  end
+  for i = 1, #glyphs[1] do
+    local line = ''
+    for j = 1, #glyphs do
+      line = line .. ' ' .. glyphs[j][i]
+    end
+    table.insert(lines, line)
+  end
+  for i = 1, padding_y do
+    table.insert(lines, '')
+  end
+
+  local padding = string.rep(' ', get_padding_x(lines))
+  for i, g in pairs(lines) do
+    lines[i] = padding .. g
+  end
+
+  return lines
+end
+
+function M.update_time()
+  local width = vim.api.nvim_win_get_width(0)
+  local height = vim.api.nvim_win_get_height(0)
+
+  -- Clock
+  local time = vim.fn.strftime('%H:%M:%S')
+  local get_padding_x = function(lines)
+    local max_len = math.max(unpack(vim.tbl_map(vim.fn.strdisplaywidth, lines)))
+    return (width - max_len)/2
+  end
+  local glyphs = M.glyph_lines(M.str_to_glyph(time), get_padding_x, 2)
+  vim.api.nvim_buf_set_lines(M.buffer, 0, #glyphs, false, glyphs)
+
+  -- Seperator
+  vim.api.nvim_buf_set_lines(M.buffer, #glyphs, #glyphs + 1, false, { string.rep('⎯', width) })
+
+  clock_endline = #glyphs + 1
+end
+
+return M
--- lua/sidekick/init.lua
+++ lua/sidekick/init.lua
@@ -1,69 +1,24 @@
-local font = require('sidekick.font')
-
-local function lines(str)
-  local t = {}
-  for line in str:gmatch('[^\r\n]+') do
-    table.insert(t, line)
-  end
-  return t
-end
-
-local M = {}
+local M = {
+  clock = require('sidekick.clock'),
+  tasks = {}
+}
 
 local win_handle = nil
 local buffer = vim.api.nvim_create_buf(false, true)
 
-function M.str_to_glyph(str)
-  local glyphs = {}
-  for ch in str:gmatch('.') do
-    if font[ch] == nil then
-      print('missing glyph for ' .. ch)
-      return
-    end
-
-    table.insert(glyphs, lines(font[ch]))
-  end
-
-  return glyphs
-end
-
-function M.glyph_lines(glyphs)
-  local lines = {}
-  for i = 1, #glyphs[1] do
-    local line = ''
-    for j = 1, #glyphs do
-      line = line .. ' ' .. glyphs[j][i]
-    end
-    table.insert(lines, line)
-  end
-  return lines
-end
-
 function M.setup()
-  -- print('setup')
-  -- local time = vim.fn.strftime('%H:%M:%S')
-  -- local glyphs = M.glyph_lines(M.str_to_glyph(time))
-  -- for _, g in pairs(glyphs) do
-  --   print(g)
-  -- end
+  M.clock.configure({ buffer = buffer })
 end
 
 function M.open()
-  print('opening')
-  local width = vim.api.nvim_win_get_width(0)
-  local height = vim.api.nvim_win_get_height(0)
+  M.clock.start()
 
-  local time = vim.fn.strftime('%H:%M:%S')
-  local glyphs = M.glyph_lines(M.str_to_glyph(time))
-
-  local str_width = vim.fn.strdisplaywidth(glyphs[1])
-  local padding = string.rep(' ', (width - str_width)/2)
-  for i, g in pairs(glyphs) do
-    glyphs[i] = padding .. g
-  end
-  vim.api.nvim_buf_set_lines(buffer, 0, #glyphs, false, glyphs)
+  vim.api.nvim_buf_set_keymap(buffer, 'n', '<Esc>', '<cmd>qa<cr>', { noremap = false, silent = true })
 
   if win_handle == nil then
+    local width = vim.api.nvim_win_get_width(0)
+    local height = vim.api.nvim_win_get_height(0)
+
     win_handle = vim.api.nvim_open_win(buffer, true, {
       relative = 'editor',
       width = width,
