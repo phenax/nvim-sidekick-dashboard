089e0489ce9ae31e4fa26d8dbc439d18f5aa4ce2
Akshay Nair <phenax5@gmail.com>
2023-12-17 17:46:21 +0530

    feat: adds bin to launch sidekick

--- test.lua
+++ test.lua
@@ -1,12 +1,3 @@
-function add_rtp(path)
-  vim.opt.rtp:append(vim.fn.expand('$HOME/.local/share/nvim/lazy/' .. path))
-end
-
--- add_rtp('plenary.nvim')
--- add_rtp('nvim-treesitter')
 vim.opt.rtp:append(vim.fn.getcwd())
 
----
-
 require('sidekick').setup({ file = './tasks.norg' }).open()
-
